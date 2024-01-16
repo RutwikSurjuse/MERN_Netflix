c83eb5b962d1fd7d87ffc13a50e1b80abd791d8b
rutwik.surjuse <rutwik20surjuse00@gmail.com>
2024-01-16 23:38:00 +0530

    Models and Routes added

--- api/app.js
+++ api/app.js
@@ -1,7 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connection with DB successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);

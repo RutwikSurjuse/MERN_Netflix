e4d2c64bfc76e9fc8afdeda2d72798e35f0d0639
Rutwik <rutwik20surjuse00@gmail.com>
2024-01-18 02:15:06 +0530

    Updated register route with fixes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,25 +2,41 @@ import express from "express";
 import User from "../models/User.js";
 import bcrypt from "bcrypt"
 
+const app = express();
+app.use(express.json());
 const router = express.Router();
 
 //REGISTER
 router.post("/register", async (req, res) => {
+  try{
   const saltRounds = 10;
+  console.log(req.body)
   const Password = req.body.password;
-  let Hash;
   bcrypt.hash(Password, saltRounds, async (err, hash) => {
-    if(err) console.log(err)
-    else Hash = hash
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: Hash,
+      password: hash, // Use the hashed password here
     });
-  })
-  const user = await newUser.save();
-  res.status(201).json(user);
-});
+
+    try {
+      const user = await newUser.save();
+      res.status(201).json(user);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+}catch(err){
+  console.log(err)
+}});
+
 
 //LOGIN
 
